d82f733165dc3387af1a03362ad2e9f5fa94a4d4
ShinnosukeUesaka <shinnosuke.uesaka@gmail.com>
2023-10-28 13:50:29 -0700

    set state

--- chapps/state.py
+++ chapps/state.py
@@ -1,12 +1,56 @@
 """Base state for the app."""
-
 import reflex as rx
 
+class User(rx.Base):
+    id: int
+    name: str
+
+class Example(rx.Base):
+    inputs: list[str]
+    output: str
+
+class Chapp(rx.Base):
+    id: int
+    title: str
+    short_description: str
+    how_to_use: str
+    icon_url: str
+    inputs: list[str]
+    examples: list[Example]
+    instruction: list[str]
 
 class State(rx.State):
     """Base state for the app.
 
     The base state is used to store general vars used throughout the app.
     """
+    user: User
+    logged_in: bool = False
 
-    pass
+    def create_user_or_login(self, name):
+        self.logged_in = True
+        pass
+
+class HomeState(State):
+    my_chapps = list[Chapp]
+
+    def get_chaps(self):
+        pass
+
+class RunChappState(State):
+    chapp: Chapp
+    inputs: list[str]
+    output: str
+    status: str # waiting_for_user_response, running, finished
+
+    def run_chapp(self):
+        pass
+
+class ExploreState(State):
+    search_results = list[Chapp]
+
+    def search_chaps(self, serach_query):
+        pass
+
+class ConfigState(State):
+    chapp: Chapp
