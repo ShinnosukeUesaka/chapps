0c57c079b68271378c452f1129687fcd3a2d37fa
ShinnosukeUesaka <shinnosuke.uesaka@gmail.com>
2023-10-29 10:52:58 -0700

    improve run screen

--- chapps/pages/run_chapp.py
+++ chapps/pages/run_chapp.py
@@ -11,6 +11,12 @@ def chapp_screen() -> rx.Component:
             rx.image(src="/robo.jpg", width="100%", height="40%", background_size="cover"),
             rx.heading(RunChappState.chapp.title, ),
             rx.text(RunChappState.chapp.description, width="90%"),
+            rx.button(
+                "Edit",
+                type_="confirm",
+                on_click=ConfigChappState.edit_chapp(RunChappState.chapp.id),
+                style=button_style,
+            ),
             width="45%",
             background_color="#EAF0F3",
             #shadow on the right
@@ -29,7 +35,6 @@ def chapp_screen() -> rx.Component:
             rx.flex(
                 rx.vstack(
                     rx.box(
-                        rx.heading("Input"),
                         rx.divider(height=10),
                         rx.foreach(RunChappState.chapp.inputs, input_field),
                         align_items="left",
@@ -41,12 +46,12 @@ def chapp_screen() -> rx.Component:
                         "Run",
                         type_="confirm",
                         on_click=RunChappState.run_chapp(),
+                        width="100px",
                         style=button_style,
                     ),
                     padding=7,
                 ),
                 rx.vstack(
-                    rx.heading("Output"),
                     rx.divider(height=10),
                     rx.box(
                         rx.markdown(RunChappState.output, width = "90%"),
@@ -54,12 +59,6 @@ def chapp_screen() -> rx.Component:
                         height="40%",
                     ),
                     rx.divider(height=10),
-                    rx.button(
-                        "Edit",
-                        type_="confirm",
-                        on_click=ConfigChappState.edit_chapp(RunChappState.chapp.id),
-                        style=button_style,
-                    ),
                     width="70%",
                     padding=7,
                     align_items="left",
